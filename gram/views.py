f5091e98368118cd89b86f5088c45d8a955bb04b
ramza007 <ramza007@gmail.com>
2018-05-10 15:49:13 +0300

    added first template

--- gram/views.py
+++ gram/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('here it comes ')
+def base(request):
+    return render(request, 'base.html')
