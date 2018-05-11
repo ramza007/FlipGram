1df20beab2720ef77b1e4f5ebcf18a6a846a3b97
ramza007 <ramza007@gmail.com>
2018-05-11 14:46:33 +0300

    added navbar buttons, added home page, added images

--- gram/urls.py
+++ gram/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.base,name = 'base'),
+    url('^home',views.base,name = 'base'),
+    url(r'^profile/', views.profile, name='profileView'),
+    url(r'^$', views.explore, name='explore')
 ]
--- gram/views.py
+++ gram/views.py
@@ -4,3 +4,16 @@ from django.http  import HttpResponse
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+
+def profile(request):
+    '''
+    View function to display the profile of the logged in user when they click on the user icon
+    '''
+    return render(request, 'my-profile.html')
+
+def explore(request):
+    '''
+    Views the initial profile
+    '''
+    return render(request, 'explore.html')
